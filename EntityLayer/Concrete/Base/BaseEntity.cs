3651ea8230ce888fa41929dc758bbcd04f21e605
aqtao <aqtao@ORXAN>
2022-11-01 15:57:44 +0400

    Add project files.

---
+++ EntityLayer/Concrete/Base/BaseEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete.Base
+{
+    public class BaseEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; } = new Guid();
+        public bool Status { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifyedDate { get; set; }
+
+        public Guid CreatUserId { get; set; }
+        public Guid ModifyUserId { get; set; }
+
+        public AppUser CreatUser { get; set; }
+        public AppUser ModifyUser { get; set; }
+    }
+}
