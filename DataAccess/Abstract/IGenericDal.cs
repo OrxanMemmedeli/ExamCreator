a3e053201d681ab0c63a51bbf1139e90a6844b7e
OrxanMemmedeli <memmedeli.orxan.om@gmail.com>
2022-11-02 15:25:53 +0400

    DAL part 1

---
+++ DataAccess/Abstract/IGenericDal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Insert(T t);
+        void Update(T t);
+        void Delete(T t);
+        Task<IQueryable<T>> GetAllAsnyc();
+        Task<T> GetByIdAsnyc(int id);
+        Task<IQueryable<T>> GetAllAsnyc(Expression<Func<T, bool>> filter);
+        Task<T> GetByAsnyc(Expression<Func<T, bool>> filter);
+
+    }
+}
---
+++ DataAccess/Repositories/GenericRepository.cs
@@ -0,0 +1,59 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Concrete.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        private readonly ECContext _context;
+
+        public GenericRepository(ECContext context)
+        {
+            _context = context;
+        }
+
+        public DbSet<T> Table => _context.Set<T>();
+
+        public void Delete(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<T>> GetAllAsnyc()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<T>> GetAllAsnyc(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByAsnyc(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsnyc(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
